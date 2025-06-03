fcd8d09054f4fb5f2d4897ed42f9908c092f9553
anrofiq <addininur.rofiqi@wingscorp.com>
2025-06-03 14:45:23 +0700

    filter helpdeskcat missing goods

--- src/order/queries/list-missing-goods/list-missing-goods.result.ts
+++ src/order/queries/list-missing-goods/list-missing-goods.result.ts
@@ -6,34 +6,40 @@ export class ListMissingGoodsResult {
         limit: props.data.limit,
         total: props.data.total,
       },
-      data: props.data.listData.map((item) => {
-        return {
-          ticket: item.header.alertNo,
-          status: item.header.alerts.find(
-            (as) => as.alertNo === item.header.alertNo,
-          )?.alertStatus,
-          date: Math.floor(new Date(item.header.createdDate).getTime() / 1000),
-          lastUpdateData: Math.max(
-            0,
-            Math.floor(new Date(item.header.changedDate).getTime() / 1000),
-          ),
-          itemsMissing: item.header.alerts.map((alert) => {
-            return {
-              ticket: alert.alertNo,
-              productDesc: item.details.find(
-                (as) => as.alertNo === alert.alertNo,
-              )?.produkDesc,
-              qty: item.details.find((as) => as.alertNo === alert.alertNo)
-                ?.jumlahSelisih,
-              uom: item.details.find((as) => as.alertNo === alert.alertNo)?.uom,
-              problem: item.header.detailProblem,
-              solution: item.header.solutions.find(
-                (as) => as.alertNo === item.header.alertNo,
-              ),
-            };
-          }),
-        };
-      }),
+      data: props.data.listData
+        .filter((s) => s.header.helpdeskCat === 6)
+        .map((item) => {
+          return {
+            cat: item.header.helpdeskCat,
+            ticket: item.header.alertNo,
+            status: item.header.alerts.find(
+              (as) => as.alertNo === item.header.alertNo,
+            )?.alertStatus,
+            date: Math.floor(
+              new Date(item.header.createdDate).getTime() / 1000,
+            ),
+            lastUpdateData: Math.max(
+              0,
+              Math.floor(new Date(item.header.changedDate).getTime() / 1000),
+            ),
+            itemsMissing: item.header.alerts.map((alert) => {
+              return {
+                ticket: alert.alertNo,
+                productDesc: item.details.find(
+                  (as) => as.alertNo === alert.alertNo,
+                )?.produkDesc,
+                qty: item.details.find((as) => as.alertNo === alert.alertNo)
+                  ?.jumlahSelisih,
+                uom: item.details.find((as) => as.alertNo === alert.alertNo)
+                  ?.uom,
+                problem: item.header.detailProblem,
+                solution: item.header.solutions.find(
+                  (as) => as.alertNo === item.header.alertNo,
+                ),
+              };
+            }),
+          };
+        }),
     };
   }
 }
